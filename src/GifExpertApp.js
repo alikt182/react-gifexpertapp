86649b364d9ca4d5cc9215bf3fb02593528c0cd2
Ali Monserrate <ali.monserrate.icg@gmail.com>
2021-09-30 15:23:42 -0400

    gifExpertApp.js  borrado comentarios

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -6,25 +6,12 @@ import { GiftGrid } from "./components/GiftGrid";
 
 export const GifExpertApp = () => {
 
-    //const categories = ['Bebidas','Sopas', 'Pasteleria'];
     const [categories, setCategories] = useState(['Bebidas']);
-
-    // const handleAdd = () =>{
-    //     //setCategories(['Postres', ...categories]); 
-    //     ////otra manera
-    //     setCategories(cats => [
-    //         ...categories, 
-    //         'Postres'
-    //     ])
-    //}
-
   return (
     <>
       <h2>GiftExpertApp</h2>
       <AddCategory setCategories={ setCategories }/>
       <hr />
-
-      {/* <button onClick={handleAdd}>Agregar</button> */}
 
       <ol>
           {
